049c18f98e4c44682bdcc6bd460a811d0d15b8a1
David Weber <dwebe003@ucr.edu>
2017-12-09 13:35:52 -0800

    organized code a bit

--- Vector ADT/main.cpp
+++ Vector ADT/main.cpp
@@ -6,79 +6,97 @@ using namespace std;
 
 int main()
 {
-    IntVector vec = IntVector(1, 4); // initializes vector
-    cout << "Vec sz: " << vec.size() << endl;
+	cout << "\nInitializing vector of size 1 with value 4...\n\n";
+    IntVector vec = IntVector(1, 4); 							// initializes vector
+	
+	// S I Z E
+	cout << "vec.size() test...\n";
+    cout << "Vec size: " << vec.size() << endl << endl;
+	
+	// E R A S E
+	cout << "vec.erase() test...\n";
     vec.erase(0);
-    cout << vec.at(0) << endl;
-    /*cout << "vec.at(4) = " << vec.at(4) << endl;        // tests at() function
-    cout << "Vec size: " << vec.size() << endl;         // tests size() function
-    cout << "Vec cap: " << vec.capacity() << endl;      // tests cap() function
-    
-    // E M P T Y
+    cout << "Vec size: " << vec.size() << endl << endl;
+	
+	// E M P T Y 
+	cout << "vec.empty() test...\n";
     if(!vec.empty())                            
     {       
-        cout << "NOT EMPTY" << endl;                    // tests empty() fctn
+        cout << "NOT EMPTY\n\n";                    			// tests empty() fctn
     }
+	else
+	{
+		cout << "EMPTY\n\n";
+	}
+	
+	// P U S H _ B A C K
+	cout << "vec.push_back(-1) and vec.push_back(500) test...\n";
+    vec.push_back(-1);                                  		// tests push_back() fn
+	vec.push_back(500);
+	
+	// A T
+	cout << "vec.at(x) test...\n";
+    cout << "vec.at(0) = " << vec.at(0) << endl;        		// tests at() function
+	cout << "vec.at(1) = " << vec.at(1) << endl;        		// tests at() function
+    cout << "Vec size: " << vec.size() << endl;         		// tests size() function
+    cout << "Vec cap: " << vec.capacity() << endl << endl;     	// tests cap() function
     
+
     // F R O N T
-    cout << "Vec front: " << vec.front() << endl;       // tests front() fctn
+	cout << "vec.front() and vec.back() test...\n";
+    cout << "Vec front: " << vec.front() << endl;       		// tests front() fctn
     // B A C K
-    cout << "Vec back: " << vec.back() << endl;         // tests back() fctn
-    
-    // P U S H _ B A C K
-    vec.push_back(-1);                                  // tests push_back() fn
-    
-    // (these make sure push_back() operated correctly)
-    cout << "New vec size: " << vec.size() << endl;
-    cout << "Vec back() after pushback: " << vec.back() << endl;
+    cout << "Vec back: " << vec.back() << endl << endl;        	// tests back() fctn
+
     
     // P O P _ B A C K
-    vec.pop_back();                                     // tests pop_back() fn
-    
-    // (these ensure pop_back() operated correctly)
-    cout << "Vec size() after popback: " << vec.size() << endl;
-    //cout << "Vec at index 5 after pop_back(): " << vec.at(5) << endl; //should abort
+	cout << "vec.pop_back(); test...\n";
+    vec.pop_back();                                     		// tests pop_back() fn
+	cout << "Vec size: " << vec.size() << endl;
+	cout << "Vec front: " << vec.front() << endl << endl;
+
     
     // C L E A R
+	cout << "vec.clear() test...\n";
     vec.clear();
-    //cout << "Vec at index 0 after clear(): " << vec.at(0) << endl; //should abort
+    cout << "Vec size: " << vec.size() << endl << endl;
     
     // R E S I Z E
-    vec.resize(6, -2);                                  // tests resize() fctn
-    
-    // (these make sure resize() operated correctly)
-    cout << "Vec front after resize: " << vec.front() << endl;
-    cout << "Vec back after resize: " << vec.back() << endl; 
+	cout << "vec.resize(6, -2) test...\n";
+    vec.resize(6, -2);                                 			 // tests resize() fctn
+	cout << "Vec size: " << vec.size() << endl;
+	cout << "Vec back: " << vec.back() << endl << endl;
+	
     
     // I N S E R T
-    vec.insert(4, 0);                                   // test insert() fctn
+	cout << "vec.insert(4, 0) test...\n";
+    vec.insert(4, 0);                                   		// test insert() fctn
+	cout << "vec.at(4) = " << vec.at(4) << endl << endl;
     
-    cout << "Vector list after clear, resize, and insert: " << endl;
-    for(unsigned i = 0; i < vec.size(); i++)
-    {
-        cout << vec.at(i) << " ";
-    }
-    cout << endl;
+    cout << "Vector list so far...\n";
+	for(unsigned i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << " ";
+	}
+	cout << endl << endl;
     
-    // E R A S E    
-    vec.erase(4);                                       // tests erase() fctn
-    cout << "After erase, vec at index 4 (where the 0 was inserted) = "
-        << vec.at(4) << endl;
     
     // A S S I G N
+	cout << "vec.assign(5, 0) test...\n";
     vec.assign(5, 0);
-    cout << "Vec list after assign(): " << endl;
+    cout << "Vector list after assign(5, 0): " << endl;
     for(unsigned i = 0; i < vec.size(); i++)
     {
         cout << vec.at(i) << " ";
     }
-    cout << endl;
+    cout << endl << endl;
     
     // R E S E R V E
+	cout << "vec.reserve(20) test...\n";
     cout << "Cap: " << vec.capacity() << endl << "...reserving..." << endl;
-    vec.reserve(20);                                    // tests reserve() fn
-    cout << "Cap: " << vec.capacity() << endl;
-    */
+    vec.reserve(20);                                    		// tests reserve() fn
+    cout << "Cap: " << vec.capacity() << endl << endl;
+    
 
     return 0;
 }
